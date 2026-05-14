ea70b2450786fe04fd1acd19843b8b30dae26307
Tylerdonahue1 <tylerjdonahue1@gmail.com>
2026-05-14 16:31:27 -0700

    Perf: switch React to production builds (~1MB saved); drop tweaks-panel in prod (~18KB); preconnect unpkg; preload key SVG assets

--- hackathon/app.jsx
+++ hackathon/app.jsx
@@ -1,12 +1,6 @@
 /* App entrypoint */
 
-const TWEAK_DEFAULTS = /*EDITMODE-BEGIN*/{
-  "showFaq": true
-}/*EDITMODE-END*/;
-
 function App() {
-  const [tweaks, setTweak] = window.useTweaks(TWEAK_DEFAULTS);
-
   return (
     <>
       <Nav />
@@ -17,18 +11,9 @@ function App() {
       <Demo />
       <AboutXPRIZE />
       <Prize />
-      {tweaks.showFaq && <Faq />}
+      <Faq />
       <CTA />
       <Footer />
-
-      <window.TweaksPanel title="Tweaks">
-        <window.TweakSection label="Sections" />
-        <window.TweakToggle
-          label="Show FAQ"
-          value={tweaks.showFaq}
-          onChange={v => setTweak("showFaq", v)}
-        />
-      </window.TweaksPanel>
     </>
   );
 }
